f33268c95f1b9268e31a6fb2fafeb45b2108b3b9
Nico Bachner <yo@nicob.dev>
2020-10-01 21:44:25 +0200

    modified build.sh

--- build.sh
+++ build.sh
@@ -5,30 +5,43 @@ PROJECT=$1
 cd $PROJECT
 pandoc ${PROJECT}.md --template=../template.latex -o ${PROJECT}.tex
 
-pdflatex ${PROJECT}.tex
-bibtex ${PROJECT}.aux
+if [ -e *.bib ] || [ -e *.bibtex ]
+then
+  pdflatex ${PROJECT}.tex
+  bibtex ${PROJECT}.aux
+fi
+
 pdflatex ${PROJECT}.tex
 pdflatex ${PROJECT}.tex
 
-if [ $2 = "keep-all" ]
+if [ "$2" == "keep-all" ]
 then
-  echo "Keeping build PROJECTs"
-elif [ $2 = "keep-tex" ]
+  echo "Keeping build files"
+elif [ "$2" == "keep-tex" ]
 then
   rm ${PROJECT}.aux
   rm ${PROJECT}.log
   rm ${PROJECT}.out
   rm ${PROJECT}.toc
-  rm ${PROJECT}.bbl
-  rm ${PROJECT}.blg
+
+  if [ -e *.bib ] || [ -e *.bibtex ]
+  then
+    rm ${PROJECT}.bbl
+    rm ${PROJECT}.blg
+  fi
+
   echo "Keeping latex document"
 else
   rm ${PROJECT}.aux
   rm ${PROJECT}.log
   rm ${PROJECT}.out
   rm ${PROJECT}.toc
-  rm ${PROJECT}.bbl
-  rm ${PROJECT}.blg
+
+  if [ -e *.bib ] || [ -e *.bibtex ]
+  then
+    rm ${PROJECT}.bbl
+    rm ${PROJECT}.blg
+  fi
   
   rm ${PROJECT}.tex
 fi
--- install.sh
+++
@@ -1,4 +0,0 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
-brew install pandoc
-brew cask install basictex
