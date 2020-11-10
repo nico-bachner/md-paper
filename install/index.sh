82953b3ed177c94416dd398eace1b99f4e590056
Nico Bachner <yo@nicob.dev>
2020-11-10 12:27:12 +0100

    reverted to destination build

--- install/index.sh
+++ install/index.sh
@@ -20,6 +20,13 @@ else
     brew install pandoc
 fi
 
+if brew ls --versions shc > /dev/null
+then
+    echo "shc was already installed"
+else
+    brew install shc
+fi
+
 if latex -v > /dev/null
 then
     echo "a TeX distribution was already installed"
@@ -29,15 +36,19 @@ fi
 
 if [ -d /usr/local/bin/md-paper ]
 then
-    curl -o https://md-paper.now.sh/dist/md-paper /usr/local/bin/md-paper
+    curl -o https://md-paper.now.sh/dist/md-paper ~/md-paper.sh
     curl -o https://md-paper.now.sh/dist/template.tex ~/template.tex
+
+    shc -f ~/md-paper.sh -o ~/md-paper
+    mv ~/md-paper /usr/local/bin/
+    rm ~/md-paper.sh
+
+    if [ -d /usr/local/bin/md-paper ] && [ -d ~/template.tex ]
+    then
+        echo "installation successful"
+    fi
 else
     echo "It seems a program called 'md-paper' already exists"
     echo "To install please rename or delete the existing program and try again" 
     exit 1
-fi
-
-if [ -d /usr/local/bin/md-paper ] && [ -d ~/template.tex ]
-then
-    echo "installation successful"
 fi
